18acbf904c11cec988b9749a62b2f30c96a5c37e
Abdullah Jan <abdullahjanazeemi@gmail.com>
2025-12-17 21:41:21 +0500

    Remove ResumeDownloadButton component and update exports
    - Delete resume-download-button.tsx file with client-side PDF generation
    - Remove ResumeDownloadButton export from pdf/index.ts

--- components/pdf/index.ts
+++ components/pdf/index.ts
@@ -1,3 +1,2 @@
 export { ResumeTemplate } from "./resume-template";
-export { ResumeDownloadButton } from "./resume-download-button";
 export { ResumeViewer, EmptyProfileState } from "./resume-viewer";
